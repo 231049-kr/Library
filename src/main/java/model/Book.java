5368bdc39c3ba5e66aa810ed35c91afda2d452b7
231019 <231019@WINDOWS-IMN57AK.mr.hi-joho.ac.jp>
2026-07-29 14:34:12 +0900

    訂正

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -4,15 +4,11 @@ public class Book {
 	
 	private String bookId;
 	private String title; 
-	private String status;
 	
 	public Book(String title,String bookId, String status) {
 		this.title = title;
 		this.bookId = bookId;
-		this.status = status;
 	}
 	public String getBookId() {return bookId;}
 	public String getTitle() {return title;}
-	public String getStatus() {return status;}
-	
 }
